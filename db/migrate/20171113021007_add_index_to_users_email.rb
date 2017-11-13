1b6727bd8a6156f536f4c2120bd9e29bf3a76ce6
Khalif <khalifaali12@gmail.com>
2017-11-12 22:06:34 -0500

    user validation is almost complete, password is broken bc of bcrypt error with ruby 2.3.3

---
+++ db/migrate/20171113021007_add_index_to_users_email.rb
@@ -0,0 +1,8 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration[5.1]
+  
+	def change
+  
+	   add_index :users, :email, unique: true
+	end
+
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+
+class UserTest < ActiveSupport::TestCase
+
+	def setup
+	   @user = User.new(name:"Example User", email:"user@example.com")
+	end
+
+	test "should be valid" do 
+	   assert @user.valid?
+	end
+
+	test "should not be valid:
+	  @user.name = "   "
+	  assert_not @user.valid?
+        end
+
+	test "name should not be too long" do
+	  @user.name = "a" * 51
+          assert_not @user.valid?
+        end
+
+	test "email should not be too long" do
+	   @user.email = "a" *244 + "example.com"
+           assert_not @user.valid?
+	end
+
+end
