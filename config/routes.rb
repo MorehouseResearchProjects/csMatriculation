ce9fc66a0fe5ceca867b3bf6c774c03a3192ba78
Khalif <khalifaali12@gmail.com>
2017-12-02 01:31:55 -0500

    broken :(

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
 
 
   root 'welcome#index'
+
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
 
   resources :users
  resources :password_resets,     only: [:new, :create, :edit, :update]
